5147f2d7d57cc73ae3ea5e5852e549e7197e625d
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-07-28 20:00:26 -0300

    feat: a

--- app/controllers/rated_controller.rb
+++ app/controllers/rated_controller.rb
@@ -3,17 +3,6 @@ class RatedController < ApplicationController
   def courses
     # Return last 10 course
     @courses = Course.limit(10).order('created_at desc')
-    render json: {
-      courses: @courses.map do |course|
-        {
-          id: course.id,
-          name: course.name,
-          description: course.description,
-          value: course.value,
-          image_url: url_for(course.image),
-          teacher: course.teacher,
-        }
-      end
-    }
+    render json: @courses
  end 
 end
