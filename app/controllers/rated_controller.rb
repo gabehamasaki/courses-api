4b82cff32ec9715ac17965d9ea1367da65f2d01c
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-07-28 18:32:18 -0300

    fix: rated/courses is a public route

--- app/controllers/rated_controller.rb
+++ app/controllers/rated_controller.rb
@@ -1,7 +1,19 @@
 class RatedController < ApplicationController
+  before_action :authorize_request, except: [:courses]
   def courses
     # Return last 10 course
     @courses = Course.limit(10).order('created_at desc')
-    render json: @courses
+    render json: {
+      courses: @courses.map do |course|
+        {
+          id: course.id,
+          name: course.name,
+          description: course.description,
+          value: course.value,
+          image_url: url_for(course.image),
+          teacher: course.teacher,
+        }
+      end
+    }
  end 
 end
